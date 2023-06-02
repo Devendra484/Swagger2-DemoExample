98f48ba33e3529f08e0564fb268d999b70f42cab
Devendra@484 <devendra.narasimhulugari@softsuave.org>
2023-06-02 11:05:22 +0530

    1st commit

---
+++ src/main/java/com/springboot/Swagger2DemoExampleApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swagger2DemoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swagger2DemoExampleApplication.class, args);
+	}
+
+}
